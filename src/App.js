22ddf75f709946491f1ae318115e7e3c27b4f0d1
John Martin <jomart@Johns-MacBook-Air.local>
2021-10-14 09:56:45 -0400

    fully clickable version with changing player

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
+import {useState} from 'react';
 
-function Square() {
+function Square(props) {
   return (
-    <button className="square">
-      
+    <button onClick={props.onClick} className="square">
+      {props.value}
     </button>
   );
 }
 
-function Board() {
+function Board(props) {
+  
   function renderSquare(i) {
-    return <Square/>;
+    return <Square onClick={() => props.clickHandler(i)} value={props.squares[i]}/>;
   }
-  const status = 'Next player: X';
+  const status = 'Next player: ' + props.currPlayer;
 
   return (
     <div>
@@ -37,10 +39,22 @@ function Board() {
 }
 
 function Game() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [currPlayer, setCurrPlayer] = useState("X")
+
+  function handleClick(i) {
+    let newSquares = squares.slice();
+    if (newSquares[i] !== currPlayer) {
+      newSquares[i] = currPlayer;
+      setSquares(newSquares);
+      setCurrPlayer(currPlayer === "X" ? "O" : "X");
+    }
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={squares} currPlayer={currPlayer} clickHandler={handleClick}/>
       </div>
       <div className="game-info">
         <div>{/* status */}</div>
